45ec844380499a529bf2946b1b59d468dc4060e8
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-07-14 17:14:34 +0200

    (VIEWS): Project, (GET): basic structure

--- keep-me-alive/routes/projects.routes.js
+++ keep-me-alive/routes/projects.routes.js
@@ -3,7 +3,13 @@ const router = require('express').Router();
 const Project = require("../models/Project.model")
 
 router.get("/", (req, res) => {
-    res.send("hello world")
+    Project.find()
+        .then(() => {
+            console.log('I can pull projects from DB')
+        })
+        .catch()
+
+    res.render("projects/projects")
 })
 
 module.exports = router
