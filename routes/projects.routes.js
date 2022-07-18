95c851fb23a3281661ef25153a32f8b88281891e
Sepideh Attar <sepideh.att@gmail.com>
2022-07-18 12:51:45 +0200

    add route for adding projects

--- routes/projects.routes.js
+++ routes/projects.routes.js
@@ -1,15 +1,60 @@
 const router = require('express').Router();
-
+const axios = require('axios')
 const Project = require("../models/Project.model")
 
 router.get("/", (req, res) => {
-    Project.find()
-        .then(() => {
-            console.log('I can pull projects from DB')
-        })
-        .catch()
-
-    res.render("projects/projects")
+  Project.find()
+    .then((projectsArr) => {
+      console.log(projectsArr)
+      res.render('projects/projects', { projects: projectsArr })
+    })
+    .catch(err => {
+      console.log("error getting project on DB", err)
+      next(err);
+    });
 })
+
+
+
+router.get("/add-project", (req, res) => {
+  res.render("projects/add-project")
+})
+
+
+router.post('/add-project', (req, res) => {
+  const {
+    endPointsLink,
+    clientSideLink,
+    activeUserName,
+    activePassword
+  } = req.body
+  // console.log(req.body, "reqqqqqqqqq")
+
+
+  Project.create({
+    endPointsLink,
+    clientSideLink,
+    activeUserName,
+    activePassword
+  })
+    .then((projectFromDb) => res.redirect("/"))
+    .catch(err => {
+      console.log("error creating Project on DB", err)
+    })
+
+})
+
+
+
+  // Project.find()
+  //   .then(projectsArr => {
+  //     projectsArr.forEach(project=>{
+  //       axios.post(project.endPointsLink,{userName: project.activeUserName,password : project.activePassword})
+  //       .then()
+  //     })
+  //   })
+
+
+
 
 module.exports = router
